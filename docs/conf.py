b022539079f257cf830959b79abd57cfb478fcc0
Garuda <106117111+garootman@users.noreply.github.com>
2025-04-09 17:02:35 +0700

    pre-commit

--- docs/conf.py
+++ docs/conf.py
@@ -9,25 +9,25 @@
 
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath(".."))
 
 
-project = 'Symbiosis API Client'
-copyright = '2025, garootman'
-author = 'garootman'
+project = "Symbiosis API Client"
+copyright = "2025, garootman"
+author = "garootman"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
