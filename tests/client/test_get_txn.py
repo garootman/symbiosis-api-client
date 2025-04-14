114c12a25f10e357679f7bcd39019dcfc474b936
Garuda <106117111+garootman@users.noreply.github.com>
2025-04-14 18:05:21 +0700

    no save data!

--- tests/client/test_get_txn.py
+++ tests/client/test_get_txn.py
@@ -13,10 +13,5 @@ def client():
 def test_get_txn(client):
     txn_hash = "0xe514ea90e4bdb3947a41497aef4e03eec2210a964518ef1657a5ec09676c7435"
     txn = client.get_txn_status(chain_name="Ethereum", txn_hash=txn_hash)
-    import json
-
-    jsondata = txn.model_dump(exclude_none=True, by_alias=True)
-    with open("123-2.json", "w") as f:
-        f.write(json.dumps(jsondata, indent=4))
     assert isinstance(txn, models.TxResponseSchema)
     assert txn.status.text == "Success"
